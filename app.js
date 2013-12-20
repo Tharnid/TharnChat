941471ce601daab52453a103a1433519bd2ea860
Tharnid <jeffmsmithdev@gmail.com>
2013-12-20 17:22:38 -0500

    done with module and testing the server

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var express = require('express');
 var io = require('socket.io');
 
-
+var chatty = require('chatty-server');
 // Express App right here
 
 var app = express();
@@ -16,60 +16,76 @@ var http = require('http').createServer(app);
 var io = require('socket.io').listen(http);  // listening to http
 // End Socket.io stuff
 
-// EventEmitter Stuff
-var EventEmitter = require('events').EventEmitter;
+// args
+var args = process.argv.slice(2);
 
-var emitter = new EventEmitter();
+console.log(args);
 
-emitter.on('hi ', function(data) {
-	console.log('someone said hi!!! ' + data);
+var server = chatty.createServer({
+	httpServer: http,
+	tcpHost: args[0],
+	tcpPort: args[1]
 });
 
-setInterval(function() {
-	emitter.emit('hi', 'emitter is emitting!!!');
-}, 1000);
+server.start();
 
-// end Emitter
-
-// listen to connections and data
-io.sockets.on('connection', function(socket) {
-	
-	// emitting event(s)
-	socket.emit('message', 'hi there client!!!');
-
-	// receiving data from socket.io client
-	socket.on('message', function(data) {
-		console.log('Data from Socket.io client: ' + data);
-	});
+server.on('client connected', function(socket) {
+	console.log('A client connected!!!');
 });
 
-// Middleware hi perf plugins
-// where the static files are located
+// // where the static files are located
 app.use(express.static(path.join(__dirname, 'static')));
 
-http.listen(1234); 
-// End Express App
+http.listen(args[2]);
 
-var server = net.Server();
 
-server.listen(3366, 'localhost');
+// // listen to connections and data
+// io.sockets.on('connection', function(socket) {
+	
+// 	// emitting event(s)
+// 	socket.emit('message', 'hi there client!!!');
 
-server.on('connection', function(socket) {
-	socket.write('Hi client!!!');
+// 	// receiving data from socket.io client
+// 	socket.on('message', function(data) {
+// 		console.log('Data from Socket.io client: ' + data);
+// 	});
+// });
 
-	socket.on('data', function(data) {
-		console.log('Data from client: ' + data);
-	});
-});
+// // Middleware hi perf plugins
+// // where the static files are located
+// app.use(express.static(path.join(__dirname, 'static')));
 
-var connection = net.connect(3366, 'localhost', function() {
-	connection.write('Hi there server!!!');
+//  
+// // End Express App
 
-	setInterval(function() {
-		connection.write('ping');
-	}, 1000 * 2);
+// var server = net.Server();
 
-	connection.on('data', function(data) {
-		console.log('Data from the server: ' + data);
-	});
-});
+// server.listen(3366, 'localhost');
+
+// server.on('connection', function(socket) {
+// 	socket.write('Hi client!!!');
+
+// 	socket.on('data', function(data) {
+// 		console.log('Data from client: ' + data);
+// 	});
+// });
+
+// var connection = net.connect(3366, 'localhost', function() {
+// 	connection.write('Hi there server!!!');
+
+// 	setInterval(function() {
+// 		connection.write('ping');
+// 	}, 1000 * 2);
+
+// 	connection.on('data', function(data) {
+// 		console.log('Data from the server: ' + data);
+// 	});
+// });
+
+// client stuff
+
+setTimeout(function() {
+	var connection = net.connect(1234, 'localhost', function() {
+
+	})
+}, 1000 * 2);
