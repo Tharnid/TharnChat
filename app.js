e7b03ca39711410ce77698a139b4ff6b5a9d210b
Tharnid <jeffmsmithdev@gmail.com>
2013-12-20 14:11:12 -0500

    done with event emitter

--- app.js
+++ app.js
@@ -1,4 +1,54 @@
+// app dependencies
 var net = require('net');
+var path = require('path');
+var express = require('express');
+var io = require('socket.io');
+
+
+// Express App right here
+
+var app = express();
+var http = require('http').createServer(app);
+
+// end Express app variable things
+
+// requiring socket.io
+var io = require('socket.io').listen(http);  // listening to http
+// End Socket.io stuff
+
+// EventEmitter Stuff
+var EventEmitter = require('events').EventEmitter;
+
+var emitter = new EventEmitter();
+
+emitter.on('hi ', function(data) {
+	console.log('someone said hi!!! ' + data);
+});
+
+setInterval(function() {
+	emitter.emit('hi', 'emitter is emitting!!!');
+}, 1000);
+
+// end Emitter
+
+// listen to connections and data
+io.sockets.on('connection', function(socket) {
+	
+	// emitting event(s)
+	socket.emit('message', 'hi there client!!!');
+
+	// receiving data from socket.io client
+	socket.on('message', function(data) {
+		console.log('Data from Socket.io client: ' + data);
+	});
+});
+
+// Middleware hi perf plugins
+// where the static files are located
+app.use(express.static(path.join(__dirname, 'static')));
+
+http.listen(1234); 
+// End Express App
 
 var server = net.Server();
 
